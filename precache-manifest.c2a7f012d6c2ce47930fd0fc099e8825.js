a451e67d357bde92610d84de5da7ea76da0ec05f
Yadnesh Khode <yadneshkhode@gmail.com>
2020-06-23 13:00:38 +0530

    Updates

---
+++ precache-manifest.c2a7f012d6c2ce47930fd0fc099e8825.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc44c16d21c4f9a06601e378550f9b62",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "15b7bc0661fb463e995d",
+    "url": "/monsters-rolodex/static/css/main.90d7b601.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b7bc0661fb463e995d",
+    "url": "/monsters-rolodex/static/js/main.11d6012f.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
